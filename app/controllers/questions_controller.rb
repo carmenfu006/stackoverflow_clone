8a83e81eb1b8d36b1c71dd68b5d022c53a246e14
carmenfu006 <guess_006@hotmail.com>
2021-06-03 00:57:36 +0800

    Enable user to create question

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,46 @@
+class QuestionsController < ApplicationController
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
+  
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params.merge(user_id: current_user.id))
+
+    if @question.save
+      redirect_to user_path(current_user), notice: 'Question was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to user_path(current_user), notice: 'Question was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to user_path(current_user), notice: 'Question was successfully destroyed.'
+  end
+
+  private
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:question).permit(:title)
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def show
+    @questions = current_user.questions.all
   end
 
 end
