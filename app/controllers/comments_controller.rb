1fb2bdf99561d0f80ccdfc6eb5a67a4799ae0363
carmenfu006 <guess_006@hotmail.com>
2021-06-09 00:28:05 +0800

    Add comment to answer with normal action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,23 @@
 class CommentsController < ApplicationController
-  def index
-    @answer = Answer.find(params[:answer_id])
-    @question = Question.find(params[:question_id])
-    @comment = Comment.new
-    @comments = @answer.comments.all
-  end
 
   def create
     @question = Question.find(params[:question_id])
     @answer = Answer.find(params[:answer_id])
     @comment = @answer.comments.new(comment_params.merge(user_id: current_user.id))
 
+    # if @comment.save
+    #   redirect_to question_path(@question), notice: 'Comment was successfully created.'
+    # else
+    #   redirect_to question_path(@question), alert: 'There was an error.'
+    # end
+
     respond_to do |format|
       if @comment.save
+        format.html { redirect_to question_path(@question), notice: 'Comment was successfully created.' }
         # # turbo frame at comments/index.html.erb
-        format.html { redirect_to question_answer_comments_path(@question, @answer), notice: 'Comment was successfully created.' }
-        # format.html { redirect_to question_answer_comment_path(@answer, @comment), notice: 'Comment was successfully created.' }
-        # format.html redirect_to question_path(@question), notice: 'Comment was successfully created.'
+        # format.html { redirect_to question_answer_comments_path(@question, @answer), notice: 'Comment was successfully created.' }
       else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: 'comments/comment_form', locals: { @comment => @comment, @question => @question, @answer => @answer })}
+        # format.turbo_stream { render turbo_stream: turbo_stream.replace(@comment, partial: 'comments/form', locals: { :@comment => @comment, :@question => @question, :@answer => @answer })}
         format.html { redirect_to question_path(@question), alert: 'There was an error.' }
       end
     end
@@ -31,7 +30,9 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to question_answer_comments_path(@question, @answer), notice: 'Comment was successfully created.' }
+      format.html { redirect_to question_path(@question), notice: 'Comment was successfully destroyed.' }
+      # # turbo frame at comments/index.html.erb
+      # format.html { redirect_to question_answer_comments_path(@question, @answer), notice: 'Comment was successfully created.' }
     end
   end
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   
-  after_create_commit { broadcast_append_to 'comments' }
-  after_update_commit { broadcast_replace_to 'comments' }
-  after_destroy_commit { broadcast_remove_to 'comments' }
+  # after_create_commit { broadcast_prepend_to 'comments' }
+  # after_update_commit { broadcast_replace_to 'comments' }
+  # after_destroy_commit { broadcast_remove_to 'comments' }
 
   validates :body, presence: true
 
